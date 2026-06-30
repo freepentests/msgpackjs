2676ef34fc09a8fea3ab6dc2fa928ff10a2ab725
freepentests <252639873+freepentests@users.noreply.github.com>
2026-06-30 18:40:34 +0100

    added objects

--- src/Modules/Encoder/index.js
+++ src/Modules/Encoder/index.js
@@ -229,6 +229,52 @@ export default class Encoder {
 		return this.bb.flip();
 	}
 
+	#writeFixMap(object) {
+		this.bb.writeUint8(0b10000000 && Object.keys(object).length);
+
+		Object.entries(object).forEach(entry => {
+			this.write(entry[0]);
+			this.write(entry[1]);
+		});
+	}
+
+	#write16BitMap(object) {
+		this.bb.writeUint8(0xde);
+		this.bb.writeUint16(Object.keys(object).length);
+
+		Object.entries(object).forEach(entry => {
+			this.write(entry[0]);
+			this.write(entry[1]);
+		});
+	}
+
+	#write32BitMap(object) {
+		this.bb.writeUint8(0xdf);
+		this.bb.writeUint32(Object.keys(object).length);
+
+		Object.entries(object).forEach(entry => {
+			this.write(entry[0]);
+			this.write(entry[1]);
+		});
+	}
+
+	writeMap(object) {
+		const numItems = Object.keys(object);
+		const isFixMap = numItems <= 15;
+		const is16BitMap = numItems >= 16 && numItems < 2 ** 16;
+		const is32BitMap = numItems >= 2 ** 16 && numItems < 2 ** 32;
+
+		if (isFixMap) {
+			this.#writeFixMap(object);
+		} else if (is16BitMap) {
+			this.#write16BitMap(object);
+		} else if (is32BitMap) {
+			this.#write32BitMap(object);
+		}
+
+		return this.bb.flip();
+	}
+
 	write(data) {
 		switch (typeof data) {
 			case 'number':
@@ -248,6 +294,7 @@ export default class Encoder {
 				if (data === null) return this.writeNull();
 				else if (data instanceof Uint8Array || data instanceof Uint8ClampedArray) return this.writeBinArray(data);
 				else if (data instanceof Array) return this.writeArray(data);
+				else return this.writeMap(data);
 				break;
 		}
 	}
