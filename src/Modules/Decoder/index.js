7c33056b0fbbb43b129e78cb5cb7e89aec416ed5
freepentests <252639873+freepentests@users.noreply.github.com>
2026-07-01 10:19:48 +0100

    added one of the timestamp types to the decoder

--- src/Modules/Decoder/index.js
+++ src/Modules/Decoder/index.js
@@ -161,9 +161,21 @@ export default class Decoder {
 		return object;
 	}
 
+	decodeTimestamp32(bytes) {
+		const bb = ByteBuffer.wrap(bytes);
+		const seconds = bb.readUint32();
+		const dateObject = new Date(seconds * 1000);
+
+		return dateObject;
+	}
+
 	decodeFixext(bb, length) {
 		const type = bb.readInt8();
 		const bytes = bb.readBytes(length).toBuffer();
+
+		if (length === 4 && type === -1) {
+			return this.decodeTimestamp32(bytes);
+		}
 
 		return new ExtData(type, new Uint8Array(bytes));
 	}
